c5808d6766ef5f1a1478663b04ad037a651ef008
Amanda Bell <amandajbell94@gmail.com>
2020-08-16 13:57:06 -0400

    First commit

---
+++ functions/contact.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+exports.handler = (event, context, callback) => {
+  const mailgun = require("mailgun-js");
+  console.log("API KEY", process.env.MAILGUN_API_KEY);
+  const mg = mailgun({
+    apiKey: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN,
+  });
+
+  const data = JSON.parse(event.body);
+
+  const email = {
+    from: "Excited User <me@samples.mailgun.org>",
+    to: `${data.name} <${data.email}>`,
+    subject: data.subject,
+    text: data.body,
+  };
+
+  mg.messages().send(email, (error, response) => {
+    callback(error, {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    });
+  });
+};
